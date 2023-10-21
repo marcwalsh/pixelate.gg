3be0e139c10aaa9400049036e4cf4e6462b3956c
Marc Walsh <marcwalsh@me.com>
2023-10-22 00:03:59 +0100

    back to basics

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 window.onload = function() {
-    // Function to set GIFs' positions and sizes
     const setGifDimensions = () => {
-        // Calculate scale factors based on the rendered image size
         const imageWidthRendered = document.querySelector('.background-layer img').clientWidth;
         const imageHeightRendered = document.querySelector('.background-layer img').clientHeight;
 
         const widthScaleFactor = imageWidthRendered / 3000;
         const heightScaleFactor = imageHeightRendered / 1714;
 
-        // Calculate the GIFs' position and dimensions
         const gifTopPosition = 420 * heightScaleFactor;
         const gifLeftPosition = 1090 * widthScaleFactor;
-        const gifWidth = 670 * widthScaleFactor;  // Width of the TV screen area (1760 - 1090)
-        const gifHeight = 600 * heightScaleFactor;  // Height of the TV screen area (1020 - 420)
+        const gifWidth = 670 * widthScaleFactor;
+        const gifHeight = 600 * heightScaleFactor;
 
-        // Set GIFs' CSS properties based on calculations
         const gifs = document.querySelectorAll('.gif-layer .gif');
         gifs.forEach(gif => {
             gif.style.top = gifTopPosition + 'px';
@@ -24,25 +20,6 @@ window.onload = function() {
         });
     }
 
-    // Set the dimensions upon initial load
     setGifDimensions();
-
-    // Update GIF dimensions upon window resize
     window.addEventListener('resize', setGifDimensions);
-
-    // Handle scroll behavior to determine which GIF to show
-    window.addEventListener('scroll', function() {
-        const scrollPercentage = (window.scrollY + window.innerHeight) / document.documentElement.scrollHeight;
-        const gifs = document.querySelectorAll('.gif-layer .gif');
-
-        // Hide all GIFs initially
-        gifs.forEach(gif => gif.classList.remove('active'));
-
-        // Determine which GIF to show based on scrollPercentage
-        if (scrollPercentage <= 0.5) { // First half of the page
-            gifs[0].classList.add('active');
-        } else if (scrollPercentage > 0.5) { // Second half of the page
-            gifs[1].classList.add('active');
-        }
-    });
 };
